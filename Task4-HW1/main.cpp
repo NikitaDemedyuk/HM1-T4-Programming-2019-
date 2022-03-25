5b0f0dbcc15d3786cddc4ec0d4bea47153fa8e21
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-25 15:07:53 +0300

    Initial Commit

---
+++ Task4-HW1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task4-HW1
+//
+//  Created by Никита on 3/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
